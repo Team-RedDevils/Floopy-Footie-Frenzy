1f817ec01fb68d656ad6815ab7d1a7849292df9c
durmusgulbahar <durmus.gulbahar@outlook.com>
2023-04-11 15:54:10 +0300

    esc menu

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-
+    public GameObject pauseMenu;
     public void PlayGame() {  
         SceneManager.LoadScene("Ozan");  
         
@@ -15,4 +15,31 @@ public class UIManager : MonoBehaviour
    public void QuitGame() {
         Application.Quit();
    }
+   
+   public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenu.activeSelf)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
 }
